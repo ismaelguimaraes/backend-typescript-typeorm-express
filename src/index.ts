c1a0a680d684da1c3dfaf4f7c1ad2742a2e21233
Ismael <Ismael@Fattorias-MacBook-Pro.local>
2020-08-15 12:32:35 -0300

    Start project

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata";
+require('dotenv').config();
+
+import express from 'express';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3334, () => {
+    console.log('Server started on port 3334!');
+});
